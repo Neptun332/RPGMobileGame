4dce28c871153ecd76be4c40c7fe4501fee63afd
Neptun332 <patrykbednarz8@gmail.com>
2019-02-26 16:15:19 +0100

    Created Project

---
+++ app/src/main/java/com/example/patry/rpgmobilegame/Loging.java
@@ -0,0 +1,13 @@
+package com.example.patry.rpgmobilegame;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Loging extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loging);
+    }
+}
