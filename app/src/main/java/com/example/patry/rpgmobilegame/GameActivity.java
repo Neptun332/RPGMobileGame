7007806509db33c388b9d36157696cc2b289c599
Neptun332 <patrykbednarz8@gmail.com>
2019-02-28 21:14:44 +0100

    merged Neptun branch and master

--- app/src/main/java/com/example/patry/rpgmobilegame/GameActivity.java
+++ app/src/main/java/com/example/patry/rpgmobilegame/GameActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import com.example.patry.rpgmobilegame.R;
 
 public class GameActivity extends AppCompatActivity {
 
