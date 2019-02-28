c09d866b39f15f0ab0aef46cc100d1655554bb63
Dosilly <dosilly.darek@gmail.com>
2019-02-28 22:58:07 +0100

    Added simple character activity

--- app/src/main/java/com/example/patry/rpgmobilegame/activities/CharacterActivity.java
+++ app/src/main/java/com/example/patry/rpgmobilegame/activities/CharacterActivity.java
@@ -1,10 +1,12 @@
 package com.example.patry.rpgmobilegame.activities;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.patry.rpgmobilegame.R;
@@ -17,17 +19,24 @@ import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreException;
 
+import java.util.Map;
+
 import javax.annotation.Nullable;
+
+import static java.lang.Math.toIntExact;
 
 public class CharacterActivity extends AppCompatActivity {
     private static final String TAG = "CharacterActivity";
 
-    private static final String KEY_NAME = "Name";
-    private static final String KEY_STR = "Strength";
-    private static final String KEY_AGL = "Agility";
-    private static final String KEY_VIT = "Vitality";
+    private static final String KEY_NAME = "name";
+    private static final String KEY_STR = "strength";
+    private static final String KEY_AGL = "agility";
+    private static final String KEY_VIT = "vitality";
 
-    private EditText characterName;
+    private TextView characterName;
+    private TextView characterStr;
+    private TextView characterAgl;
+    private TextView characterVit;
     //temp
     private String UID = "mfQUhMQHJs2iNPiPK7rc";
 
@@ -41,17 +50,17 @@ public class CharacterActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_character);
 
-        characterName =  findViewById(R.id.NameText);
-
-        //loadCharacterData();
-
+        characterName =  findViewById(R.id.nameText);
+        characterStr = findViewById(R.id.strText);
+        characterAgl = findViewById(R.id.aglText);
+        characterVit = findViewById(R.id.vitText);
     }
-
     // Listener for any data changes in DB, if something changes view will be updated
     @Override
     protected void onStart(){
         super.onStart();
         charRef.addSnapshotListener(this, new EventListener<DocumentSnapshot>() {
+            @RequiresApi(api = Build.VERSION_CODES.N)
             @Override
             public void onEvent(@Nullable DocumentSnapshot documentSnapshot, @Nullable FirebaseFirestoreException e) {
                 if(e != null) {
@@ -61,15 +70,20 @@ public class CharacterActivity extends AppCompatActivity {
                 }
 
                 if(documentSnapshot.exists()) {
-                    character = new Character(documentSnapshot.getString(KEY_NAME),10,10,10);
-                    characterName.setText(character.name);
+                    Map<String, Object> charMap = documentSnapshot.getData();
+                    character = new Character((String) charMap.get(KEY_NAME),toIntExact((long)charMap.get(KEY_STR)),toIntExact((long)charMap.get(KEY_AGL)),toIntExact((long)charMap.get(KEY_VIT)));
+                   // character = new Character((String) charMap.get(KEY_NAME),10,10,10);
+                    characterName.setText("Name: " + character.name);
+                    characterStr.setText("Strength: " + character.strength);
+                    characterAgl.setText("Agility: " + character.agility);
+                    characterVit.setText("Vitality: " + character.vitality);
+
                 } else {
                     Toast.makeText(CharacterActivity.this, "document does not exists", Toast.LENGTH_SHORT);
                 }
             }
         });
     }
-
     void loadCharacterData() {
         charRef.get()
                 .addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
