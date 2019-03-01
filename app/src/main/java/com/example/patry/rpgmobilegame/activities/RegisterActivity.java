e1e8716b4322e90490ab118b880fd57958928d7b
Neptun332 <patrykbednarz8@gmail.com>
2019-03-01 20:08:31 +0100

    added auth login, added login in fields

--- app/src/main/java/com/example/patry/rpgmobilegame/activities/RegisterActivity.java
+++ app/src/main/java/com/example/patry/rpgmobilegame/activities/RegisterActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -12,11 +13,19 @@ import android.widget.Toast;
 
 import com.example.patry.rpgmobilegame.R;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class RegisterActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private static final String TAG = "RegisterActivity";
 
     private Button registerButton;
     private EditText nicknameEditText;
@@ -27,6 +36,7 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
     private ProgressDialog progressDialog;
 
     private FirebaseAuth firebaseAuth;
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -79,13 +89,37 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (task.isSuccessful()) {
                             Toast.makeText(RegisterActivity.this, "User Registered", Toast.LENGTH_SHORT).show();
+                            AddUserToDatabase();
                             Intent intent = new Intent(RegisterActivity.this, GameActivity.class);
                             startActivity(intent);
                         } else {
                             Toast.makeText(RegisterActivity.this, "Error " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                            Log.d(TAG,task.getException().toString());
                         }
                     }
                 });
+
+    }
+
+    private void AddUserToDatabase() {
+//        Map<String, Object> note = new HashMap<>();
+//        note.put(KEY_TITLE, title);
+//        note.put(KEY_DESCRIPTION, description);
+//
+//        db.collection("Notebook").document("My First Note").set(note)
+//                .addOnSuccessListener(new OnSuccessListener<Void>() {
+//                    @Override
+//                    public void onSuccess(Void aVoid) {
+//                        Toast.makeText(MainActivity.this, "Note saved", Toast.LENGTH_SHORT).show();
+//                    }
+//                })
+//                .addOnFailureListener(new OnFailureListener() {
+//                    @Override
+//                    public void onFailure(@NonNull Exception e) {
+//                        Toast.makeText(MainActivity.this, "Error!", Toast.LENGTH_SHORT).show();
+//                        Log.d(TAG, e.toString());
+//                    }
+//                });
 
     }
 
